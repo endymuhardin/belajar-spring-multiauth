e5139a48db9f15946776740e7830b2ef879b257f
Endy Muhardin <endy.muhardin@gmail.com>
2020-05-13 08:58:31 +0700

    starter project

---
+++ src/main/java/com/muhardin/endy/belajar/spring/security/multiauth/BelajarMultiauthApplication.java
@@ -0,0 +1,13 @@
+package com.muhardin.endy.belajar.spring.security.multiauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BelajarMultiauthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BelajarMultiauthApplication.class, args);
+	}
+
+}
